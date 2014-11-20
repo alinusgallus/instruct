91358b7c8a0c0639134fc0f233679c92239997ca
alinusgallus <gall.alain@gmail.com>
2014-11-20 15:26:12 +0800

    initiate instruct

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Instruct::Application.config.secret_key_base = '6cfad8dea14d7c0709f2cb35bf6a2588f5601085a912796b6a7114e96580b7a9cffb820f133fcd43deec66239f69236c86762eb6f86cf523eea54b85ee8923c9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Instruct::Application.config.session_store :cookie_store, key: '_instruct_session'
